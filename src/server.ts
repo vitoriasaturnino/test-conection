ee3d81a9accd4f0d352b579747a698a08e890988
Vitária Moura <vim.saturnino@gmail.com>
2022-10-08 17:12:39 -0300

    feature: database conection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { json } from "express";
+import { db } from "./database/db";
 
 const app =  express();
 
 app.use(json());
 
 app.listen(3001, async () => {
+  await db.sync();
   console.log('Rodando em localhost:3001');
 }); 
